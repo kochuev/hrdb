158fdb8a7209a06211ba2ace18274a8db0e7bf5a
SergiiGoncharov <web4travel@gmail.com>
2018-04-24 05:18:45 -0700

    Refactor seed.js

--- server/config/seed.js
+++ server/config/seed.js
@@ -65,22 +65,90 @@ User.find({}).removeAsync()
     });
   });
 
+
 // TODO: find out if there is any reason to use methodAsync, mongoose model.remove() for example return Query,
 // TODO: it is not a Promise, but it has .then() and can be use as Promise */
 
 
-let populatedData = {};
-Origin.find({})
-    .remove()
-    .then(() => Origin.create({
-        name: 'Test origin'
-    }))
-    .then((origins) => {
-        console.log('finished populating origins');
-        populatedData.origins = origins;
-    })
-    .then(() => Position.find({}).remove())
-    .then(() => Position.create({
+let seedCollection = function (Collection, items){
+
+    return Collection.find({})
+        .remove()
+        .then(() => Collection.create(items));
+
+};
+
+
+let seedCandidates = function(){
+
+    let candidates = [];
+    let dates = [
+        //Jan
+        '01/06/2018', '01/07/2018', '01/08/2018', '01/09/2018', '01/10/2018',
+        // March
+        '03/01/2018', '03/02/2018', '03/03/2018', '03/04/2018', '03/05/2018',
+        '03/06/2018', '03/07/2018', '03/08/2018', '03/09/2018', '03/10/2018',
+        '03/11/2018', '03/12/2018', '03/13/2018', '03/14/2018', '03/15/2018',
+        '03/16/2018', '03/17/2018', '03/18/2018', '03/19/2018', '03/20/2018',
+
+        // April
+        '04/06/2018', '04/07/2018', '04/08/2018', '04/09/2018', '04/10/2018',
+        '04/11/2018', '04/12/2018', '04/13/2018', '04/14/2018', '04/15/2018',
+
+        // May
+        '05/06/2018', '05/07/2018', '05/08/2018', '05/09/2018', '05/10/2018',
+        '05/11/2018', '05/12/2018', '05/13/2018', '05/14/2018', '05/15/2018',
+    ]
+        .map(dateStr => new Date(dateStr));
+
+    Promise.all([
+        Agency.find({}),
+        Position.find({}),
+        Origin.find({})
+    ])
+        .then((values) => {
+            // TODO: Is there better way then rely on order and indexes?
+            let agencies = values[0];
+            let positions = values[1];
+            let origins = values[2];
+
+            // TODO: Make sure these nested loops are best solution
+            dates.forEach(date => {
+                agencies.forEach(agency => {
+                    positions.forEach(position => {
+                        origins.forEach(origin => {
+
+                            let formattedDate = moment(date).format('MM/DD/YYYY');
+                            candidates.push({
+                                firstName: `James (${position.name}/${formattedDate}/${agency.name}/${origin.name})`,
+                                lastName: 'Bond',
+                                visits: [{
+                                    general: {
+                                        date: date,
+                                        _agency: agency._id,
+                                        _position: position._id,
+                                        _origin: origin._id
+                                    },
+                                    active: true
+                                }]
+                            });
+
+                        });
+                    });
+                });
+            });
+
+            return seedCollection(Candidate, candidates);
+        });
+
+
+};
+
+
+Promise.all([
+    seedCollection(Origin, [{name: 'Test origin'}]),
+    seedCollection(Agency, [{name: 'Test Agency'}]),
+    seedCollection(Position, [{
         'name': 'Symfony developer'
     },{
         'name': 'Angular developer'
@@ -88,78 +156,9 @@ Origin.find({})
         'name': 'React developer'
     },{
         'name': 'IT recruiter'
-    }))
+    }])
+])
+    .then(() => seedCandidates())
     .then(() => {
-        console.log('finished populating positions');
-    })
-    .then(() => Position.find({}))
-    .then((positions) => {
-        populatedData.positions = positions;
-    })
-    .then(() => Agency.find({}).remove())
-    .then(() => Agency.create({
-        name: 'Test agency'
-    }))
-    .then((agencies) => {
-        console.log('finished populating agencies');
-        populatedData.agencies = agencies;
-    })
-    .then(() => Candidate.find({}).remove())
-    .then(() => {
-        let candidates = [];
-
-        let dates = [
-            //Jan
-            '01/06/2018', '01/07/2018', '01/08/2018', '01/09/2018', '01/10/2018',
-            // March
-            '03/01/2018', '03/02/2018', '03/03/2018', '03/04/2018', '03/05/2018',
-            '03/06/2018', '03/07/2018', '03/08/2018', '03/09/2018', '03/10/2018',
-            '03/11/2018', '03/12/2018', '03/13/2018', '03/14/2018', '03/15/2018',
-            '03/16/2018', '03/17/2018', '03/18/2018', '03/19/2018', '03/20/2018',
-
-            // April
-            '04/06/2018', '04/07/2018', '04/08/2018', '04/09/2018', '04/10/2018',
-            '04/11/2018', '04/12/2018', '04/13/2018', '04/14/2018', '04/15/2018',
-
-            // May
-            '05/06/2018', '05/07/2018', '05/08/2018', '05/09/2018', '05/10/2018',
-            '05/11/2018', '05/12/2018', '05/13/2018', '05/14/2018', '05/15/2018',
-        ]
-            .map(dateStr => new Date(dateStr));
-
-        let agencies = Array.isArray(populatedData.agencies) ? populatedData.agencies : [populatedData.agencies];
-        let positions = Array.isArray(populatedData.positions) ? populatedData.positions : [populatedData.positions];
-        let origins = Array.isArray(populatedData.origins) ? populatedData.origins : [populatedData.origins];
-
-        // TODO: Make sure these nested loops are best solution
-        dates.forEach(date => {
-            agencies.forEach(agency => {
-                positions.forEach(position => {
-                    origins.forEach(origin => {
-
-                        let formattedDate = moment(date).format('MM/DD/YYYY');
-                        candidates.push({
-                            firstName: `James (${position.name}/${formattedDate}/${agency.name}/${origin.name})`,
-                            lastName: 'Bond',
-                            visits: [{
-                                general: {
-                                    date: date,
-                                    _agency: agency._id,
-                                    _position: position._id,
-                                    _origin: origin._id
-                                },
-                                active: true
-                            }]
-                        });
-
-                    });
-                });
-            });
-        });
-
-        return Candidate.create(candidates);
-    })
-    .then(() => Candidate.find({}))
-    .then((candidatesFound) => {
-        console.log('Candidates found after populating: ' + candidatesFound.length);
+        console.log('Finished populating Origin, Agency, Position, Candidate');
     });
