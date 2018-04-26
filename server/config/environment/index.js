7426ebd4df635a18d4d397f0af66ccb745af0743
SegiiGoncharov <web4travel@gmail.com>
2018-04-27 02:09:21 +0300

    Update seedDB in server/config/environment/index.js

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -25,7 +25,7 @@ var all = {
   ip: process.env.IP || '0.0.0.0',
 
   // Should we populate the DB with sample data?
-  seedDB: true,
+  seedDB: false,
 
   // upload dir
   uploadDir: path.normalize(__dirname + '/../../..' + '/uploads'),
