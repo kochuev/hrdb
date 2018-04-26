fe1622f1b192c16814ffcdf8c35a1f5778108748
SegiiGoncharov <web4travel@gmail.com>
2018-04-27 02:15:19 +0300

    Set validator middleware

--- server/api/stats/stats.validator.js
+++ server/api/stats/stats.validator.js
@@ -1,5 +1,48 @@
 
 export function isVisitsQueryValid(req, res, next){
-    console.log('valid');
+
+    let invalidProps = 0;
+    let messages = [];
+
+    // positions
+    if(req.query.positions){
+        let pattern = new RegExp('^[a-f\\d]{24}$', 'i'); // match 24 symbols hexadecimal string
+        let positionsInvalidMessage = 'The positions parameter is not valid, it should be 24 symbols hexadecimal string.';
+        if(Array.isArray(req.query.positions)){
+            if(!req.query.positions.every(position => pattern.test(position))){
+                invalidProps++;
+                messages.push(positionsInvalidMessage);
+            }
+        }else{
+            if(!pattern.test(req.query.positions)){
+                invalidProps++;
+                messages.push(positionsInvalidMessage);
+            }
+        }
+    }else{
+        invalidProps++;
+        messages.push('The positions parameter is required, specify at least one');
+    }
+
+    // startDate and endDate
+    let isValidDate = (date) => typeof date === 'string' && /^\d{2}-\d{2}-\d{4}$/.test(date);
+    if(req.query.startDate){
+        if(!isValidDate(req.query.startDate)){
+            invalidProps++;
+            messages.push('The startDate parameter is not valid, example of valid value is 01-01-2001.');
+        }
+    }
+    if(req.query.endDate){
+        if(!isValidDate(req.query.endDate)){
+            invalidProps++;
+            messages.push('The endDate parameter is not valid, example of valid value is 01-01-2001.');
+        }
+    }
+
+    if(invalidProps){
+        res.status(400).send(messages.join(' \n'));
+        return;
+    }
+
     return next();
 }
