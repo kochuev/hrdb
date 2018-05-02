1da43a9140e28015dfe32fe654c56071cfb38297
SergiiGoncharov <web4travel@gmail.com>
2018-05-02 20:18:23 +0300

    Add date picker to stats-date-filter component

--- client/app/stats/stats-date-filter/stats-date-filter.component.js
+++ client/app/stats/stats-date-filter/stats-date-filter.component.js
@@ -11,11 +11,50 @@
     templateUrl: 'app/stats/stats-date-filter/stats-date-filter.html',
     controller: class StatsDateFilterComponent {
 
+      constructor() {
+        this.datePicker = {
+          date: {
+            startDate: this.startDate,
+            endDate: this.endDate
+          },
+          options: {
+            ranges: {
+              'This Month': [moment().startOf('month'), moment().endOf('month')],
+              'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+              'Last 6 Months': [moment().subtract(6, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+              'This Year': [moment().startOf('year'), moment().endOf('year')],
+              'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')],
+              'All time': [moment('05-04-2001', 'DD/MM/YYYY'), moment()]
+            },
+            alwaysShowCalendars: false,
+            opens: 'left',
+            eventHandlers: {
+              'apply.daterangepicker': (event) => {
+                this.onChangeDaterangepicker(event);
+              },
+              'cancel.daterangepicker': (event) => {
+                this.onChangeDaterangepicker(event);
+              }
+            }
+          }
+        };
+
+      }
+
       onChange() {
         this.onDatesChanged({
           $event: {
-            startDate: this.startDate,
-            endDate: this.endDate
+            startDate: this.datePicker.date.startDate,
+            endDate: this.datePicker.date.endDate
+          }
+        });
+      }
+
+      onChangeDaterangepicker(event) {
+        this.onDatesChanged({
+          $event: {
+            startDate: event.model.startDate,
+            endDate: event.model.endDate
           }
         });
       }
